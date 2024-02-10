be829ebbfe018706b06f636eef784a3c49538ad8
Karnik Ram <karnikram@gmail.com>
2024-02-10 11:16:54 +0100

    Add exception handling

--- train.py
+++ train.py
@@ -119,8 +119,14 @@ def train(gpu, args):
                     flow_grad = torch.autograd.grad(flo_loss, model.module.update.gru.convq_glo.weight, retain_graph=True)[0].norm().item()
                     pose_grad = torch.autograd.grad(geo_loss, model.module.update.gru.convq_glo.weight, retain_graph=True)[0].norm().item()
                     res_grad = torch.autograd.grad(res_loss, model.module.update.gru.convq_glo.weight, retain_graph=True)[0].norm().item()
-                    flow_coeff_ratios = flow_coeff_ratios[-50:] + [max(min(pose_grad/flow_grad, 10*flow_coeff), 0.1*flow_coeff)]
-                    res_coeff_ratios = res_coeff_ratios[-50:] + [max(min(pose_grad/res_grad, 10*res_coeff), 0.1*res_coeff)]
+                    try:
+                        flow_coeff_ratios = flow_coeff_ratios[-50:] + [max(min(pose_grad/flow_grad, 10*flow_coeff), 0.1*flow_coeff)]
+                    except:
+                        flow_coeff_ratios = flow_coeff_ratios[-50:] + [max(10*flow_coeff, 0.1*flow_coeff)]
+                    try:
+                        res_coeff_ratios = res_coeff_ratios[-50:] + [max(min(pose_grad/res_grad, 10*res_coeff), 0.1*res_coeff)]
+                    except:
+                        res_coeff_ratios = res_coeff_ratios[-50:] + [max(10*res_coeff, 0.1*res_coeff)]
                     res_coeff = np.mean(res_coeff_ratios)
                     flow_coeff = np.mean(flow_coeff_ratios)
 
